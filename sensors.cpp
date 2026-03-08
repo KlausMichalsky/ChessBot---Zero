2a74cd193ae992016d84fd800b0ed2965cab18cf
KlausMichalsky <k.michalsky81@googlemail.com>
2026-03-08 18:41:44 +0100

    sensorHomingOffset

--- sensors.cpp
+++ sensors.cpp
@@ -77,3 +77,73 @@ void sensorStreamAngle(TwoWire &wire, float &lastSentAngle, unsigned long &lastS
     degrees = round1Decimal(degrees);                                          // Redondear a 1 decimal
     sendFilteredFloat(degrees, lastSentAngle, lastSendTime, 0.5, 33, Serial1); // Enviar angulo filtrado por UART
 }
+
+// ⚠️ No muy exato:
+// si un valor se desvia mucho por ruido el promedio es inexacto
+// por otra parte si la lectura esta entre el 0° y 360°
+// entonces el promedio da un valor muy fuera del rango
+// float sensorHomingOffset()
+// {
+//     const uint8_t samples = 30;
+//     float sum = 0;
+//     for (uint8_t i = 0; i < samples; i++)
+//     {
+//         sum += sensorReadAngle(Wire);
+//         delay(2); // para evitar saturar el I2C
+//     }
+//     float average = sum / samples;
+
+//     return average;
+// }
+
+// Si alguna vez tu homing mecanico cae cerca del 0 digital del sensor:
+// entonces el promedio normal se rompe
+// Mejor solucion:
+// 30 lecturas + corrección si cruza 0° + promedio 👀💡
+
+//         0°
+//    330°     30°
+//  300°         60°
+//  270°         90°
+//  240°        120°
+//   210°      150°
+//         180°
+
+float sensorHomingOffset()
+{
+    const uint8_t samples = 30;
+    float sum = 0;
+    float firstAngle = sensorReadAngle(Wire);
+
+    for (uint8_t i = 0; i < samples; i++)
+    {
+        float angle = sensorReadAngle(Wire);
+        // corregir salto 0° / 360°
+        // fabs -> valor absoluto |angle - firstAngle|
+        // Si la diferencia es mayor que 180°,
+        // significa que cruzamos el cero del sensor.
+        // si cruzamos el cero
+        // mover los valores al mismo lado del círculo
+        if (fabs(angle - firstAngle) > 180)
+        {
+            if (angle < firstAngle)
+                angle += 360;
+            else
+                angle -= 360;
+        }
+        sum += angle;
+    }
+
+    // Normalizar el angulo si el promedio queda fuera del rango
+    // Eso solo mueve el número al rango correcto
+    // Si un cálculo da 361° eso en realidad es lo mismo que 1°
+    // si pasa de 360 → restar 360, si es menor que 0 → sumar 360
+    float avg = sum / samples;
+
+    if (avg >= 360)
+        avg -= 360;
+    if (avg < 0)
+        avg += 360;
+
+    return avg;
+}
