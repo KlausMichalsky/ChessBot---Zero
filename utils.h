4123e5122aa1b66d0d6cff276f88276fa6b79867
Klaus Michalsky <k.michalsky81@googlemail.com>
2026-04-26 11:37:47 +0200

    inverse kinematics

--- utils.h
+++ utils.h
@@ -27,3 +27,5 @@ void sendFilteredFloat(float value, float &lastValue, unsigned long &lastTime,
 float normalizeAngle(float angle);
 
 bool boardToXY(const char *square, float &x, float &y);
+
+bool inverseKinematics(float x, float y, float &theta1, float &theta2);
